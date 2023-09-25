8f2cd32a7291788dfdf9d90d57c5bd0c8853f854
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2023-09-26 04:04:12 +0600

    Donate button functionality added + localstorage data has been set + Toastify added on button click update

--- src/components/DonationDetailsCard/DonationDetailsCard.jsx
+++ src/components/DonationDetailsCard/DonationDetailsCard.jsx
@@ -1,57 +1,51 @@
 import PropTypes from "prop-types";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const DonationDetailsCard = ({ donation }) => {
-  const {  image_cover, title, description, text_color, price } = donation;
+  const { id, image_cover, title, description, text_color, price } = donation;
 
+  const notifySuccess = () => toast.success("Added Donation Successfully!");
+  const notifyWarning = () => toast.error("Opps! You Already This Donation!"); // demo 2
 
- const  handleAddToDonation = () =>{
+  const handleAddToDonation = () => {
     const storedDonationArray = [];
 
-    const donationItems = JSON.parse(localStorage.getItem("donations"))
+    const donationItems = JSON.parse(localStorage.getItem("donations"));
 
-  
     if (!donationItems) {
-        storedDonationArray.push(donation);
-      localStorage.setItem('donations',JSON.stringify(storedDonationArray)  )
-    }else{
-
-        const isExists = favouriteItem.find(phone=>phone.id == id)
-        if(!isExists){
-           
-            addedFavArray.push(...favouriteItem,phone)
-            localStorage.setItem('favorites',JSON.stringify(addedFavArray))
-            Swal.fire({
-                color: 'green',
-                timer: 4000,
-                title: 'Yeah Added',
-                text: 'Success',
-                icon: 'success',
-                confirmButtonText: 'Cool'
-              })
-        }else{
-            console.log("Error!!Already in !");
-        }
-
-        }
- }
+      storedDonationArray.push(donation);
+      localStorage.setItem("donations", JSON.stringify(storedDonationArray));
+    } else {
+      const isExists = donationItems.find((donation) => donation.id == id);
+      if (!isExists) {
+        storedDonationArray.push(...donationItems, donation);
+        localStorage.setItem("donations", JSON.stringify(storedDonationArray));
+        notifySuccess();
+      } else {
+        notifyWarning();
+      }
+    }
+  };
   return (
     <>
       <div className="card w-full">
         <figure className="">
-          <img src={image_cover} alt="donation-card-cover" className="w-full rounded-xl" />
+          <img
+            src={image_cover}
+            alt="donation-card-cover"
+            className="w-full rounded-xl"
+          />
         </figure>
         <div className="relative -top-11 md:-top-32 px-4 py-[9px] md:p-10 bg-[#1111116c] z-10 rounded-b-xl">
-    
-        <button onClick={handleAddToDonation}
-          className="btn btn-xs md:btn-md w-fit text-white font-semibold border-none"
-          style={{ backgroundColor: text_color }}
-        >
-          Donate $ {price}
-        </button>
-      
+          <button
+            onClick={handleAddToDonation}
+            className="btn btn-xs md:btn-md w-fit text-white font-semibold border-none"
+            style={{ backgroundColor: text_color }}
+          >
+            Donate $ {price}
+          </button>
         </div>
-        
       </div>
 
       <div>
@@ -60,10 +54,11 @@ const DonationDetailsCard = ({ donation }) => {
           <p className="text-slate-600 leading-7">{description}</p>
         </div>
       </div>
+      <ToastContainer position="top-center" theme="colored" />
     </>
   );
 };
 DonationDetailsCard.propTypes = {
-    donation: PropTypes.object.isRequired,
-  };
+  donation: PropTypes.object,
+};
 export default DonationDetailsCard;
